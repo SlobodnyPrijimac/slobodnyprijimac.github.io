1b974536df5813f05d0f00d665fe258d66e18e03
Slobodný Prijímač <slobodnyprijimac@yandex.com>
2018-11-12 23:52:04 +0100

    First test release

---
+++ precache-manifest.6b01169921dac53e3d7e46e44d518d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b61fa315b7f171a23271453cd240afcf",
+    "url": "/static/media/slobodny-vysielac-300xwhite.b61fa315.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11cefb16cf44b7e2e91f",
+    "url": "/static/js/main.11cefb16.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "11cefb16cf44b7e2e91f",
+    "url": "/static/css/main.790c4749.chunk.css"
+  },
+  {
+    "revision": "4382364fa5c8b6155c5dea06a26d0f41",
+    "url": "/index.html"
+  }
+];
